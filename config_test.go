6b8ca96642b98631b09bbb76d3b06dc8f812f1dc
Adam Sven Johnson <adam@pkqk.net>
2021-09-08 09:55:34 +1200

    Use literals and inplace calls where possible
    Makes the tests a bit simpler I think

--- config_test.go
+++ config_test.go
@@ -7,36 +7,33 @@ import (
 )
 
 func TestConfig(t *testing.T) {
-	t.Run("no interface provided", func(t *testing.T) {
-		cfg := new(Config)
-		cfg.GatewayPort = 8082
-		cfg.PrivatePort = 8083
-		cfg.MetricsPort = 8084
-		gAddress := cfg.GatewayAddress()
-		require.Equal(t, ":8082", gAddress)
-		pAddress := cfg.PrivateAddress()
-		require.Equal(t, ":8083", pAddress)
-		mAddress := cfg.MetricAddress()
-		require.Equal(t, ":8084", mAddress)
+	t.Run("port provided", func(t *testing.T) {
+		cfg := &Config{
+			GatewayPort: 8082,
+			PrivatePort: 8083,
+			MetricsPort: 8084,
+		}
+		require.Equal(t, ":8082", cfg.GatewayAddress())
+		require.Equal(t, ":8083", cfg.PrivateAddress())
+		require.Equal(t, ":8084", cfg.MetricAddress())
 	})
-	t.Run("network address provided", func(t *testing.T) {
-		cfg := new(Config)
-		cfg.GatewayListenAddress = "0.0.0.0:8082"
-		cfg.GatewayPort = 0
-		cfg.PrivateListenAddress = "127.0.0.1:8084"
-		cfg.PrivatePort = 8083
-		cfg.MetricsListenAddress = ""
-		cfg.MetricsPort = 8084
-		gAddress := cfg.GatewayAddress()
-		require.Equal(t, "0.0.0.0:8082", gAddress)
-		pAddress := cfg.PrivateAddress()
-		require.Equal(t, "127.0.0.1:8084", pAddress)
-		mAddress := cfg.MetricAddress()
-		require.Equal(t, ":8084", mAddress)
+	t.Run("address provided and prefered over port", func(t *testing.T) {
+		cfg := &Config{
+			GatewayListenAddress: "0.0.0.0:8082",
+			GatewayPort:          0,
+			PrivateListenAddress: "127.0.0.1:8084",
+			PrivatePort:          8083,
+			MetricsListenAddress: "",
+			MetricsPort:          8084,
+		}
+		require.Equal(t, "0.0.0.0:8082", cfg.GatewayAddress())
+		require.Equal(t, "127.0.0.1:8084", cfg.PrivateAddress())
+		require.Equal(t, ":8084", cfg.MetricAddress())
 	})
 	t.Run("private http address for plugin services", func(t *testing.T) {
-		cfg := new(Config)
-		cfg.PrivatePort = 8083
+		cfg := &Config{
+			PrivatePort: 8083,
+		}
 		require.Equal(t, "http://localhost:8083/plugin", cfg.PrivateHttpAddress("plugin"))
 		cfg.PrivateListenAddress = "127.0.0.1:8084"
 		require.Equal(t, "http://127.0.0.1:8084/plugin", cfg.PrivateHttpAddress("plugin"))
