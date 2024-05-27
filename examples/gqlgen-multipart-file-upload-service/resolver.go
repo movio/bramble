5e7894cf45d3cdd8d21f2822cbdba61d16d4f652
alex <alex_star@live.ru>
2024-05-27 11:51:13 +0300

    add file upload example service

---
+++ examples/gqlgen-multipart-file-upload-service/resolver.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	_ "embed"
+	"fmt"
+	"net/http"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+)
+
+var name = "gqlgen-service"
+var version = "0.1.0"
+
+//go:embed schema.graphql
+var schema string
+
+func newResolver() http.Handler {
+	c := Config{
+		Resolvers: &Resolver{
+			service: Service{
+				Name:    name,
+				Version: version,
+				Schema:  schema,
+			},
+		},
+		Directives: DirectiveRoot{
+			// Support the @boundary directive as a no-op
+			Boundary: func(ctx context.Context, obj interface{}, next graphql.Resolver) (res interface{}, err error) {
+				return next(ctx)
+			},
+		},
+	}
+	return handler.NewDefaultServer(NewExecutableSchema(c))
+}
+
+type Resolver struct {
+	service Service
+}
+
+func (r *Resolver) Query() QueryResolver {
+	return r
+}
+
+func (r *Resolver) Mutation() MutationResolver {
+	return r
+}
+
+func (r *Resolver) Service(ctx context.Context) (*Service, error) {
+	return &r.service, nil
+}
+
+func (r *Resolver) UploadGizmoFile(ctx context.Context, upload graphql.Upload) (*string, error) {
+    fmt.Println(upload.Filename)
+	return nil, nil
+}
+func (r *Resolver) UploadGadgetFile(ctx context.Context, upload GadgetInput) (*string, error) {
+    fmt.Println(upload.Upload.Filename)
+	return nil, nil
+}
