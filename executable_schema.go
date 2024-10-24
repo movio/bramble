fc20027745442de909550242b941c1ceb2e17108
Adam Sven Johnson <adam@pkqk.net>
2024-10-25 10:08:32 +1300

    Remove loop variable aliasing prevention
    This issue is fixed in go1.22
    https://tip.golang.org/doc/go1.22#language

--- executable_schema.go
+++ executable_schema.go
@@ -103,9 +103,7 @@ func (s *ExecutableSchema) UpdateSchema(ctx context.Context, forceRebuild bool) 
 	// Avoid fetching more than 64 servides in parallel,
 	// as high concurrency can actually hurt performance
 	group.SetLimit(64)
-	for url_, s_ := range s.Services {
-		url := url_
-		s := s_
+	for url, s := range s.Services {
 		group.Go(func() error {
 			logger := log.WithField("url", url)
 			updated, err := s.Update(ctx)
