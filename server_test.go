4e7d02d4029792ba8a899f77c7c1cf4895086c26
Nima Shahri <NShahri@users.noreply.github.com>
2026-02-09 15:05:25 +1300

    Add test of  boundary fragment spread failure

--- server_test.go
+++ server_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"testing"
 
+	testsrv1 "github.com/movio/bramble/test_srv_boundary_fragment/service1"
+	testsrv2 "github.com/movio/bramble/test_srv_boundary_fragment/service2"
 	"github.com/movio/bramble/testsrv"
 	"github.com/stretchr/testify/require"
 	"github.com/vektah/gqlparser/v2"
@@ -157,4 +159,60 @@ func TestFederatedQueryWithMultipleFragmentSpreads(t *testing.T) {
 
 	gizmoService.Close()
 	gadgetService.Close()
+
+}
+
+func TestFederatedQueryWithBoundary(t *testing.T) {
+	gizmoService := testsrv1.NewGizmoService()
+	gadgetService := testsrv2.NewGadgetService()
+
+	executableSchema := NewExecutableSchema(nil, 10, nil, NewService(gizmoService.URL), NewService(gadgetService.URL))
+
+	require.NoError(t, executableSchema.UpdateSchema(context.TODO(), true))
+
+	t.Run("It should resolve boundary fields on fragments correctly", func(t *testing.T) {
+		query := gqlparser.MustLoadQuery(executableSchema.MergedSchema, `{
+			gizmo(id: "GIZMO1") {
+				id
+				gadget {
+					... on Jetpack {
+						name	
+						range
+						description
+						__typename
+					}
+					... on InvisibleCar {
+						name
+						cloaked
+						performance
+						__typename
+					}
+				}
+			}
+		}`)
+
+		ctx := testContextWithoutVariables(query.Operations[0])
+
+		response := executableSchema.ExecuteQuery(ctx)
+		expectedResponse := `
+		{
+			"gizmo": {
+				"id": "GIZMO1",
+				"gadget": {
+					"name": "Jetpack #1",
+					"range": "500km",
+					"description":"Jetpack #1 Description",
+					"__typename": "Jetpack"
+				}
+			}
+		}`
+
+		// Assert that response.Errors is empty
+		require.Empty(t, response.Errors)
+		jsonEqWithOrder(t, expectedResponse, string(response.Data))
+	})
+
+	gizmoService.Close()
+	gadgetService.Close()
+
 }
